54426de31e11699f50ce456e6b80ff70c231d98d
Ellen Bledsoe <ellen.bledsoe@ufl.edu>
2021-05-03 17:21:09 -0600

    start working script for session 1

---
+++ scripts/session1.R
@@ -0,0 +1,9 @@
+# PACKAGES and DATA #
+
+# load the tidyverse (mostly for tidyr, dplyr, and ggplot2)
+library(tidyverse)
+
+# load in data from URL
+dugout_data <- read_csv("https://raw.githubusercontent.com/bleds22e/FAST_lab_training/master/data/Dugout_master%202017.csv")
+
+# DATA EXPLORATION #
