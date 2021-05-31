f5267d5ed7ed6f1507b5f05f7ceb32fa3b65c948
Ellen Bledsoe <ellen.bledsoe@ufl.edu>
2021-05-31 16:39:10 -0600

    initialize ggplot2 script

---
+++ scripts/Session3_ggplot2.R
@@ -0,0 +1,9 @@
+##### INTRO TO PLOTTING WITH GGPLOT2 #####
+# ggplot2 overview: https://ggplot2.tidyverse.org/
+# ggplot2 extensions: https://exts.ggplot2.tidyverse.org/gallery/
+# Data Carpentry ggplot lesson: 
+#     https://datacarpentry.org/R-ecology-lesson/04-visualization-ggplot2.html
+
+# use dugout/wetland data from 2 different years for faceting?
+# lots of things to use for scatter plots...
+# can use year or D/W for categorical variable for box/violin plots
